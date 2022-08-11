daeba2ab726a0de09ae690b4df5b44713c892267
JoanVigne <100919931+JoanVigne@users.noreply.github.com>
2022-08-11 11:04:10 +0100

    Add files via upload

---
+++ js/level.js
@@ -0,0 +1,70 @@
+function touchDowm(){
+    let level = document.title;
+    switch(level){
+        case "index" : console.log("Menu principal");
+        break;
+        case "First Level" : console.log("Level One");
+        document.title = "Level 2";
+        break;
+        case "Level 2" : console.log("Level One");
+        break;
+        case "Level 3" : console.log("Level One");
+        break;
+        case "Level 4" : console.log("Level One");
+        break;
+        case "Level 5" : console.log("Level One");
+        break;
+        case "Level 6" : console.log("Level One");
+        break;
+        case "Level 7" : console.log("Level One");
+        break;
+        case "Level 8" : console.log("Level One");
+        break;
+        case "Level 9" : console.log("Level One");
+        break;
+        case "Level 10" : console.log("Level One");
+        break;
+        case "Level 11" : console.log("Level One");
+        break;
+        case "Level 12" : console.log("Level One");
+        break;
+        case "Level 13" : console.log("Level One");
+        break;
+    }
+}
+
+
+// function newLevel(){
+//     let level = document.title;
+//     switch(level){
+//         case "index" : console.log("Menu principal");
+//         break;
+//         case "First Level" : console.log("Level One");
+//         break;
+//         case "First 2" : console.log("Level One");
+//         break;
+//         case "First 3" : console.log("Level One");
+//         break;
+//         case "First 4" : console.log("Level One");
+//         break;
+//         case "First 5" : console.log("Level One");
+//         break;
+//         case "First 6" : console.log("Level One");
+//         break;
+//         case "First 7" : console.log("Level One");
+//         break;
+//         case "First 8" : console.log("Level One");
+//         break;
+//         case "First 9" : console.log("Level One");
+//         break;
+//         case "First 10" : console.log("Level One");
+//         break;
+//         case "First 11" : console.log("Level One");
+//         break;
+//         case "First 12" : console.log("Level One");
+//         break;
+//         case "First 13" : console.log("Level One");
+//         break;
+//     }
+// }
+
---
+++ js/move.js
@@ -0,0 +1,64 @@
+
+
+
+// touches directionnelles
+document.onkeydown = checkKey;
+function checkKey(e) {
+    let player = document.getElementById("player");
+    const playerStyle = window.getComputedStyle(player, null);
+    let positionColumn = playerStyle.getPropertyValue("grid-column-start");
+    let positionRow = playerStyle.getPropertyValue("grid-row-start");
+    e = e || window.event;
+    if (e.keyCode == '38') {
+        if (positionRow > 1) {
+            player.style.gridRowStart = Number(positionRow) - 1;
+            console.log("touched");
+        }
+        else {
+            bulle("Trop haut");
+        }
+    }
+    else if (e.keyCode == '40') {
+        if (positionRow < 6) {
+            player.style.gridRowStart = Number(positionRow) + 1;
+            console.log("vers le bas");
+        }
+        else if (positionRow == 6) {
+            player.style.gridRowStart = Number(positionRow) + 1;
+            bulle("TOUCH DOWN");
+            touchDowm();
+        }
+    }
+    else if (e.keyCode == '37') {
+        if (positionColumn > 1) {
+            player.style.gridColumnStart = Number(positionColumn) - 1;;
+            console.log("touched");
+        }
+        else {
+            bulle("Trop a gauche");
+        }
+    }
+    else if (e.keyCode == '39') {
+        if (positionColumn < 4) {
+            player.style.gridColumnStart = Number(positionColumn) + 1;
+            console.log("touched");
+        }
+        else {
+            bulle("trop à droite !");
+        }
+    } 
+}
+
+function bulle(message) {
+    console.log(message);
+    let bulle = document.createElement("p");
+    bulle.classList.add("bulle");
+    bulle.innerHTML = message;
+    player.append(bulle);
+    setTimeout(function(){ effacerBulle(); }, 1300);
+    function effacerBulle(){
+        let toutesLesBulles = document.getElementsByClassName("bulle");
+            toutesLesBulles[0].parentNode.removeChild(bulle);
+        }
+}
+
